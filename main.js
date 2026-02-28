0ce39de0fa03461c75c930e6f453ea8ef0d1ce83
alwannelson <alwanaditiaduha@gmail.com>
2026-02-28 22:58:02 +0700

    fix directory main.js

--- main.js
+++ main.js
@@ -2,16 +2,16 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const path = require('path')
-const route = require('./routes/router.js')
+const route = require('./src/routes/router.js')
 require('dotenv').config()
 
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, './Apps/views'))
+app.set('views', path.join(__dirname, './src/Apps/views'))
 
 app.use(expressLayouts)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
-app.use(express.static(path.join(__dirname, './Apps/public')))
+app.use(express.static(path.join(__dirname, './src/Apps/public')))
 
 app.use('/', route)
 app.use('/', (req, res) => {
